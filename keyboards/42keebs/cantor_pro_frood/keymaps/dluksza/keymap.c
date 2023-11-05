f602febb94a0aacc7643ac7f2e50c4023df7204c
Dariusz Luksza <dariusz.luksza@gmail.com>
2023-11-05 19:57:20 +0000

    Reformat layer definition code

--- keyboards/42keebs/cantor_pro_frood/keymaps/dluksza/keymap.c
+++ keyboards/42keebs/cantor_pro_frood/keymaps/dluksza/keymap.c
@@ -53,10 +53,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                       └───────┘   └───┘
       */
     [_QW] = LAYOUT_split_3x6_3(
-           PAREN,    KC_Q,   KC_W,  KC_E,  KC_R,  KC_T,                         KC_Y,  KC_U,  KC_I,     KC_O,    KC_P,      BRCKE,
-           KC_GRV,   A_SFT,  KC_S,  KC_D,  KC_F,  KC_G,                         KC_H,  KC_J,  KC_K,     KC_L,    SCLN_SFT,  KC_QUOT,
-           MOD_TAB,  KC_Z,   KC_X,  KC_C,  KC_V,  KC_B,                         KC_N,  KC_M,  KC_COMM,  KC_DOT,  SLSH_NUM,  KC_ENT,
-                                KC_LGUI,  KC_BSPC,  ESC_SYM,      KC_LCTL,  SPC_NAV,  KC_RALT
+           PAREN,    KC_Q,   KC_W,  KC_E,  KC_R,  KC_T,              KC_Y,  KC_U,  KC_I,     KC_O,    KC_P,      BRCKE,
+           KC_GRV,   A_SFT,  KC_S,  KC_D,  KC_F,  KC_G,              KC_H,  KC_J,  KC_K,     KC_L,    SCLN_SFT,  KC_QUOT,
+           MOD_TAB,  KC_Z,   KC_X,  KC_C,  KC_V,  KC_B,              KC_N,  KC_M,  KC_COMM,  KC_DOT,  SLSH_NUM,  KC_ENT,
+                                KC_LGUI,  KC_BSPC,  ESC_SYM,     KC_LCTL,  SPC_NAV,  KC_RALT
     ),
      /*                        NUMBERS (/)
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
@@ -73,10 +73,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                       └───┘   └───┘
       */
     [_NUM] = LAYOUT_split_3x6_3( // numbers
-           KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    LSFT(KC_MINS),  KC_7,    KC_8,  KC_9,  KC_BSLS,        CRBRC,
-           KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_PEQL,        KC_4,    KC_5,  KC_6,  LSFT(KC_BSLS),  KC_PPLS,
-           KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_MINS,        KC_1,    KC_2,  KC_3,  KC_TRNS,        KC_TRNS,
-                                            KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_0
+           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                LSFT(KC_MINS),  KC_7,  KC_8,  KC_9,  KC_BSLS,        CRBRC,
+           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                KC_PEQL,        KC_4,  KC_5,  KC_6,  LSFT(KC_BSLS),  KC_PPLS,
+           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                KC_MINS,        KC_1,  KC_2,  KC_3,  KC_TRNS,        KC_TRNS,
+                                              KC_TRNS,  KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_0
     ),
      /*                        SYMBOLS (Esc)
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬────┬────┬────┬───┬───┐
@@ -91,11 +91,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *               └───┤   ├───┐   ┌───┤   ├───┘
       *                   └───┤ ⇓ │   │   ├───┘
       *                       └───┘   └───┘
-      */    [_SYM] = LAYOUT_split_3x6_3( // special keys like F1, F2 etc.
-           KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_F7,    KC_F8,  KC_F9,  KC_F10,  KC_TRNS,
-           KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_F4,    KC_F5,  KC_F6,  KC_F11,  KC_TRNS,
-           KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_F1,    KC_F2,  KC_F3,  KC_F12,  KC_TRNS,
-                                            KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS
+      */
+    [_SYM] = LAYOUT_split_3x6_3( // special keys like F1, F2 etc.
+           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                KC_TRNS,  KC_F7,  KC_F8,  KC_F9,  KC_F10,  KC_TRNS,
+           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                KC_TRNS,  KC_F4,  KC_F5,  KC_F6,  KC_F11,  KC_TRNS,
+           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                KC_TRNS,  KC_F1,  KC_F2,  KC_F3,  KC_F12,  KC_TRNS,
+                                                KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS
     ),
      /*                       NAVIGATION ( )
       * ┌───┬───┬───┬───┬───┬───┐       ┌────┬───┬────┬────┬────┬────┐
@@ -110,11 +111,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *               └───┤   ├───┐   ┌───┤ ⇓ ├───┘
       *                   └───┤   │   │   ├───┘
       *                       └───┘   └───┘
-      */    [_NAV] = LAYOUT_split_3x6_3(
-        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_PGUP,    KC_HOME,    KC_END,   KC_TRNS,   TPREV,    TNEXT,
-        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT,  BBACK,    BFORW,
-        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_PGDN,    KC_TRNS,    KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_PEQL,
-                                            KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS
+      */
+    [_NAV] = LAYOUT_split_3x6_3(
+           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,               KC_PGUP,  KC_HOME,  KC_END,   KC_TRNS,   TPREV,    TNEXT,
+           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,               KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  BBACK,    BFORW,
+           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,               KC_PGDN,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_PEQL,
+                                                KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
 
